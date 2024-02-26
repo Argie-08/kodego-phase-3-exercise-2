fc782c849e2383e4f988ff1d6fa1c2a41e515d48
Argie-08 <argcanonayon@gmail.com>
2024-02-26 16:17:43 +0800

    2nd

--- p1.js
+++ p1.js
@@ -1,7 +1,23 @@
-const str = ["hello", "world"];
-const chc = "l";
+// const str = ["hello", "world"];
+// const chc = "l";
 
 // so confusing what to do sir :)
 
-// const answer = countInstance(["hello", "world"], "l")
-// console.log(answer) ---> "[2, 1]"
+function countChar(arr, char) {
+  let result = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    let count = 0;
+    for (let j = 0; j < arr[i].length; j++) {
+      if (arr[i][j] === char) {
+        count++;
+      }
+    }
+    result.push(count);
+  }
+  return result;
+}
+const strings = ["hello", "world"];
+const character = "l";
+const counts = countChar(strings, character);
+console.log(counts);
