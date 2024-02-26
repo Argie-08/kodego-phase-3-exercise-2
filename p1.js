4b711bed4a1f9db9fd90d68f15301710b2a8b92b
Argie-08 <argcanonayon@gmail.com>
2024-02-26 16:27:10 +0800

    3rd

--- p1.js
+++ p1.js
@@ -1,23 +1,31 @@
-// const str = ["hello", "world"];
-// const chc = "l";
-
 // so confusing what to do sir :)
 
-function countChar(arr, char) {
-  let result = [];
+// function countChar(arr, char) {
+//   let result = [];
 
-  for (let i = 0; i < arr.length; i++) {
-    let count = 0;
-    for (let j = 0; j < arr[i].length; j++) {
-      if (arr[i][j] === char) {
-        count++;
-      }
-    }
-    result.push(count);
-  }
-  return result;
-}
+//   for (let i = 0; i < arr.length; i++) {
+//     let count = 0;
+//     for (let j = 0; j < arr[i].length; j++) {
+//       if (arr[i][j] === char) {
+//         count++;
+//       }
+//     }
+//     result.push(count);
+//   }
+//   return result;
+// }
+// const strings = ["hello", "world"];
+// const character = "l";
+// const counts = countChar(strings, character);
+// console.log(counts);
+
 const strings = ["hello", "world"];
-const character = "l";
-const counts = countChar(strings, character);
-console.log(counts);
+const toCount = "l";
+
+function countChar(string, character) {
+  return string.map((str) => {
+    return str.split(character).length - 1;
+  });
+}
+const usage = countChar(strings, toCount);
+console.log(usage);
